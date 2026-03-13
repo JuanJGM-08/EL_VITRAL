fbbe72d075430393601c42f346e6747c610fbd98
Simon <simonuwusierra@gmail.com>
2026-03-12 21:40:13 -0500

    Implementacion del modelo de usuario

---
+++ backend/src/models/Usuario.js
@@ -0,0 +1,54 @@
+const { DataTypes } = require('sequelize');
+const sequelize = require('../config/database');
+
+const Usuario = sequelize.define('Usuario', {
+    id: {
+        type: DataTypes.INTEGER,
+        primaryKey: true,
+        autoIncrement: true
+    },
+    nombre: {
+        type: DataTypes.STRING(100),
+        allowNull: false
+    },
+    email: {
+        type: DataTypes.STRING(100),
+        allowNull: false,
+        unique: true,
+        validate: {isEmail: true}
+    },
+    password: {
+        type: DataTypes.STRING(255),
+        allowNull: false
+    },
+    telefono: {
+        type: DataTypes.STRING(10)
+    },
+    direccion: {
+        type: DataTypes.TEXT
+    },
+    rol: {
+        type: DataTypes.ENUM('usuario', 'admin'),
+        defaultValue: 'usuario'
+    },
+    aprobado: {
+        type: DataTypes.DATE,
+        defaultValue: false
+    },
+    fecha_registro: {
+        type: DataTypes.DATE,
+        defaultValue: DataTypes.NOW
+    },
+    ultimo_acceso: {
+        type: DataTypes.DATE
+    },
+    activo: {
+        type: DataTypes.BOOLEAN,
+        defaultValue: true
+    }
+}, {
+    tableName: 'usuarios',
+    timestamps: false
+}
+);
+module.exports = Usuario;
